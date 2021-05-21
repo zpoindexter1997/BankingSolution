1ed9b58a66133f6c286c79432d8efbbb38fb4042
ZhaQuon Poindexter <zpoindexter@tql.com>
2021-05-21 11:58:56 -0400

    Add project files.

---
+++ Banking/Account.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking
+{
+    class Account
+    {
+        public string AccountNumber { get; private set; }
+        public decimal Balance { get; private set; }
+        public string RoutingNumber { get; set; }
+        public string Description { get; set; }
+
+        public bool Deposit(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine($"Amount must be > 0.");
+                return false;
+            }
+            Balance = Balance + amount;
+            return true;
+        }
+        public bool Withdraw(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine($"Amount must be > 0.");
+                return false;
+            }
+            if (amount > Balance)
+            {
+                Console.WriteLine($"Insufficient Funds.");
+                return false;
+            }
+            Balance = Balance - amount;
+            return true;
+        }
+        public bool Transfer(decimal amount, Account toAccount)
+        {
+            var success = Withdraw(amount);
+            if (success == true)
+            {
+                toAccount.Deposit(amount);
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Banking/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Banking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var acct1 = new Account();
+            acct1.Deposit(500);
+            acct1.Withdraw(200);
+            acct1.Withdraw(600);
+            acct1.Withdraw(-100);
+            Console.WriteLine($"Balance is {acct1.Balance}");
+
+            var acct2 = new Account();
+            acct1.Transfer(1000, acct2);
+            Console.WriteLine($"Balance is {acct1.Balance}");
+            Console.WriteLine($"Balance is {acct2.Balance}");
+        }
+    }
+}
